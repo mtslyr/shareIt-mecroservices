71f0b0c084b00e4e106150313e5fd7bb5a4b09f9
Mikhail Tsilyurik <mtsilyurik@yandex.ru>
2026-06-22 12:35:29 +0500

    rm unused classes from gateway

--- gateway/src/main/java/ru/practicum/shareit/booking/exception/BookingNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.booking.exception;
-
-import ru.practicum.shareit.common.exception.NotFoundException;
-
-public class BookingNotFoundException extends NotFoundException {
-    public BookingNotFoundException(Long bookingId) {
-        super("Бронирование с ID = %d не найдено".formatted(bookingId));
-    }
-}
--- gateway/src/main/java/ru/practicum/shareit/common/exception/OwnItemException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class OwnItemException extends ApiException {
-    public OwnItemException() {
-        super("Невозможно для собственных вещей", HttpStatus.CONFLICT);
-    }
-}
